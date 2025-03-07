40417ab9cb57d544116462c6ea03a7a419e8728d
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-07 10:15:38 -0600

    1

--- assets/php/Login.php
+++ assets/php/Login.php
@@ -1,5 +1,5 @@
 <?php
-require '../../Private/Credentials/DataBase/connection.php';
+require '../../../Private/Credentials/DataBase/connection.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
