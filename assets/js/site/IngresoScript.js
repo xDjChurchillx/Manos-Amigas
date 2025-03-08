f4eaeacf781eb0fb24c6602adce125048575cd7c
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-08 02:32:27 -0600

    1

--- assets/js/site/IngresoScript.js
+++ assets/js/site/IngresoScript.js
@@ -10,10 +10,10 @@ if (error) {
     errorMessage.style.color = 'red'; // Opcional: darle color al texto
     switch (error) {
         case '1':
-            errorMessage.textContent = 'Usuario no encontrado.';
+            errorMessage.textContent = 'Credenciales invalidas.';
             break;
         case '2':
-            errorMessage.textContent = 'Contrase\u00F1a incorrecta.';
+            errorMessage.textContent = 'Porfavor introducir credenciales.';
             break;
         case '3':
             errorMessage.textContent = 'Error en la base de datos. Inténtalo más tarde.';
