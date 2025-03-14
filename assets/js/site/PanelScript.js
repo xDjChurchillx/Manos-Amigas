7c6d2751ca5ccfdfbf322c6277445e6bcabb3fd0
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-13 22:50:55 -0600

    1

--- assets/js/site/PanelScript.js
+++ assets/js/site/PanelScript.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Función para verificar la sesión
-    function checkSession() {
+    function startSession() {
         // Realizar la solicitud AJAX
         fetch('../assets/php/panel.php')
             .then(response => {
@@ -16,29 +16,20 @@ document.addEventListener("DOMContentLoaded", function () {
                     // Si la sesión es válida, mostrar el nombre del usuario en el panel
                     document.getElementById('panel').innerHTML = `<h1>Bienvenido, ${data.user}</h1>`;
                 } else {
-                    // Si la sesión no es válida, manejar el error y redirigir al login
-                    handleError(data.message);
+                    console.error('Error iniciando la sesión:', data.message);
+                    window.location.href = 'ingreso.html';
                 }
             })
             .catch(error => {
                 // En caso de error (fallo en la solicitud o procesamiento), mostrar el error
                 console.error('Error al verificar la sesión:', error);
-                handleError(error.message || 'Error inesperado. Por favor intenta más tarde.');
+                // Redirigir al login
+                window.location.href = 'ingreso.html';
             });
     }
 
-    // Función para manejar errores y mostrar mensajes
-    function handleError(errorMessage) {
-        // Mostrar mensaje de error en consola (para debug)
-        console.log("Error:", errorMessage);
-
-        // Puedes personalizar este mensaje como un modal o un elemento en el DOM
-        alert(errorMessage);
-
-        // Redirigir al login
-        window.location.href = 'ingreso.html';
-    }
+   
 
     // Llamar a la función de verificación de sesión al cargar la página
-    checkSession();
+    startSession();
 });
