8edeb632cfd34ba6ebf0331fbd3b7303049ab3ea
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-14 17:26:03 -0600

    1

--- assets/php/Actividades.php
+++ assets/php/Actividades.php
@@ -1,17 +1,17 @@
-<?php
+ï»¿<?php
 header('Content-Type: application/json');
 
 // Simular "base de datos" de servicios/actividades
 $activitys = [
     [
         "id" => 1,
-        "title" => "Tour en Montaña",
-        "description" => "Disfruta de una caminata por la montaña con guías expertos.",
+        "title" => "Tour en MontaÃ±a",
+        "description" => "Disfruta de una caminata por la montaÃ±a con guÃ­as expertos.",
         "cardImage" => "assets/images/montana.jpg",
         "image" => "assets/images/montana_grande.jpg",
         "details" => [
-            "Guías certificados",
-            "Duración de 3 horas",
+            "GuÃ­as certificados",
+            "DuraciÃ³n de 3 horas",
             "Incluye refrigerio",
             "Paisajes espectaculares"
         ]
@@ -19,17 +19,17 @@ $activitys = [
     [
         "id" => 2,
         "title" => "Paseo en Kayak",
-        "description" => "Navega por los ríos y disfruta de la naturaleza.",
+        "description" => "Navega por los rÃ­os y disfruta de la naturaleza.",
         "cardImage" => "assets/images/kayak.jpg",
         "image" => "assets/images/kayak_grande.jpg",
         "details" => [
             "Equipos incluidos",
-            "Guías profesionales",
+            "GuÃ­as profesionales",
             "Apto para principiantes",
             "Seguro incluido"
         ]
     ],
-    // Agrega más actividades según sea necesario
+    // Agrega mÃ¡s actividades segÃºn sea necesario
 ];
 
 // Devolver como JSON
