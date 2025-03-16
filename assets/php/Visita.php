9428d1a5e0f375ca7302fb52dc64ba438c04d9aa
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-15 19:04:55 -0600

    1

--- assets/php/Visita.php
+++ assets/php/Visita.php
@@ -5,7 +5,7 @@ session_start();
 try {
     // Verificar si ya se registró una visita en los últimos 30 segundos
     if (isset($_SESSION['last_visit_update']) && (time() - $_SESSION['last_visit_update']) < 7200) {
-        echo '2';
+        echo 'false';
         exit(); // Salir sin hacer nada
     } else {
         // Llamar al procedimiento almacenado sin parámetros
@@ -14,7 +14,7 @@ try {
 
         // Registrar el tiempo de la última visita
         $_SESSION['last_visit_update'] = time();
-        echo '1';
+        echo 'true';
         exit(); // Salir correctamente
     }
 } catch (Exception $ex) {  
