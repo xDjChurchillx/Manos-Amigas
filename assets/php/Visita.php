5afbff5fae9b39f307d081034d2eb6d684da744b
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-15 18:06:22 -0600

    1

---
+++ assets/php/Visita.php
@@ -0,0 +1,21 @@
+<?php
+require '../../../Private/Credentials/DataBase/connection.php'; // Asumo que es MySQLi
+session_start();
+
+try {
+    // Verificar si ya se registró una visita en los últimos 30 segundos
+    if (isset($_SESSION['last_visit_update']) && (time() - $_SESSION['last_visit_update']) < 7200) {
+        exit(); // Salir sin hacer nada
+    } else {
+        // Llamar al procedimiento almacenado sin parámetros
+        $stmt = $conn->prepare("CALL sp_Visitas()");
+        $stmt->execute(); // Ejecutar el procedimiento
+
+        // Registrar el tiempo de la última visita
+        $_SESSION['last_visit_update'] = time();
+        exit(); // Salir correctamente
+    }
+} catch (Exception $ex) {    
+    exit();
+}
+?>
