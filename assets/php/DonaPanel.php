77cbfb3b370b776deba7672a6572c3718dda55fc
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-03-28 08:23:20 -0600

    1

--- assets/php/DonaPanel.php
+++ assets/php/DonaPanel.php
@@ -143,11 +143,7 @@ if (empty($rows)) {
             <tbody>';
 
     foreach ($rows as $Donacion) {
-       // Truncar el mensaje si es muy largo (ej: 50 caracteres)
-    $mensaje = $Donacion['Mensaje'];
-    $mensajeTruncado = (strlen($mensaje) > 50) 
-                       ? substr($mensaje, 0, 50) . '...' 
-                       : $mensaje);
+       
         $panel .= '
             <tr>
                 <td>' . htmlspecialchars($Donacion['Codigo']) . '</td>
@@ -156,7 +152,7 @@ if (empty($rows)) {
                 <td>' . htmlspecialchars($Donacion['Destino']) . '</td>
                 <td>' . htmlspecialchars($Donacion['Nombre']) . '</td>
                 <td>' . htmlspecialchars($Donacion['Contacto']) . '</td>
-                 <td>' . $mensajeTruncado . '</td>
+                <td class="text-truncate">' . htmlspecialchars($Donacion['Mensaje']) . '</td>
                 <td>    
                     <div class="d-flex justify-content-center align-items-center">
                         <button class="btn btn-primary btn-sm" onclick="edit(\'' . htmlspecialchars($Donacion['Codigo']) . '\')">Detalles</button>
