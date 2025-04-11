23cb68a0b73ac418878b1c319890142c81d4bd85
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-04-11 15:22:32 -0600

    1

---
+++ assets/js/site/InicioScript.js
@@ -0,0 +1,38 @@
+﻿// JavaScript source code
+document.getElementById('subscriptionForm').addEventListener('submit', function (e) {
+   
+    const Correo = document.getElementById('Correo');
+   
+    const errorCorreo = document.getElementById('errCorreo');
+ 
+
+    errorCorreo.textContent = '';
+   
+
+    if (Correo.value == '') {
+        errorCorreo.textContent = 'Por favor, indique un correo.';      
+        e.preventDefault();
+    }
+ });
+
+// Obtener parámetros de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const error = urlParams.get('error');
+const url = new URL(window.location.href);
+url.searchParams.delete('error');
+window.history.replaceState({}, document.title, url);
+if (error) {
+    switch (error) {
+        case '0':
+
+            break;
+        case '1':
+            Alerta('Los campos obligatorios no fueron completados');
+            break;
+        case '2':
+            Alerta('Error en servidor');
+            break;
+        default:
+
+    }
+}
---
+++ assets/php/Suscripciones.php
@@ -0,0 +1,43 @@
+﻿<?php
+    require '../../../Private/Credentials/DataBase/connection.php';
+// Verificar si el formulario fue enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $Correo = isset($_POST['Correo']) ? $_POST['Correo'] : '';
+  
+    if (empty($Correo)) {
+         header("Location: /Voluntariado.html?error=1"); // Error en 
+         exit();
+    }
+ 
+    $Correo = htmlentities($Correo, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+ 
+ 
+  $stmt = $conn->prepare('CALL sp_CrearVoluntario(?,?,?,?,?,?)');
+  if (!$stmt) {
+      header("Location: /index.html?error=2"); // Error en BD
+      exit();
+  }
+
+  $stmt->bind_param('s',$Correo, );
+
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row = $result->fetch_assoc();
+
+  if (array_key_exists('Success', $row)) {
+         header("Location: /index.html?error=0"); // Success
+         exit();
+  } else {
+      header("Location: /index.html?error=2"); // Error en BD
+      exit();
+  }
+  $stmt->close();
+  $conn->close();
+
+
+} else {
+    // Si alguien intenta acceder directamente al script sin enviar el formulario
+    header('Location: ../../index.html');
+    exit;
+}
