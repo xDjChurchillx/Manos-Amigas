5e3f4eb3470e53e233e28c0991df818c51e3621f
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-04-16 00:32:20 -0600

    1

--- assets/php/AnularSuscripcion.php
+++ assets/php/AnularSuscripcion.php
@@ -68,7 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>'Anular'</title>
+                    <title>Anular</title>
             </head>
             <body>
             <header style="
