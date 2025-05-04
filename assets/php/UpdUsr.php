e4a0dff783912a2b5c213705ee968e3a87234d32
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2025-05-04 12:40:33 -0600

    1

--- assets/php/UpdUsr.php
+++ assets/php/UpdUsr.php
@@ -48,7 +48,7 @@ try{
         $code4 = trim($_POST['code4'] ?? '');
         $code5 = trim($_POST['code5'] ?? '');
 
-        echo json_encode(["status" => "error", "ex" => "codigos:".$code1.$code2.$code3.$code4.$code5]);
+        echo json_encode(["status" => "error", "ex" => "codigos:".$code1.$code2.$code3.$code4.$code5."lengs".strlen($code1).strlen($code2).strlen($code3).strlen($code4).strlen($code5)]);
         exit();
 
         // Validación de datos
